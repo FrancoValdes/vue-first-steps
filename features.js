5bad9f17192effcfeaa6fbf4a2256d11b1c536a6
Franco <franco.valdes.p@gmail.com>
2019-11-28 15:07:52 -0300

    keyup y computed basic

--- features.js
+++ features.js
@@ -1,12 +1,33 @@
 const app = new Vue({
     el: '#app',
     data: {
-        fondo: 'bg-danger',
-        color: true
+        information: 'Fruits',
+        frutas: [
+            { nombre: 'manzana', cantidad: 0 },
+            { nombre: 'platano', cantidad: 6 },
+            { nombre: 'naranja', cantidad: 16 }
+        ],
+        nuevaFruta: '',
+        cantidad: 0,
+        total: 0
     },
     methods: {
-        cambiarColor() {
-            this.color = !this.color
+        agregarFruta() {
+            this.frutas.push({
+                nombre: this.nuevaFruta,
+                cantidad: this.cantidad
+            })
+            this.nuevaFruta = ''
+            this.cantidad = 0
         }
     },
+    computed: {
+        sumarFrutas(){
+            this.total = 0
+            for(fruta of this.frutas){
+                this.total += fruta.cantidad
+            }
+        }
+    },
+    
 })
