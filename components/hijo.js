bfe6b04b49abc0a9fafccdd001050d52a4aa17ab
Franco <franco.valdes.p@gmail.com>
2019-12-01 18:54:01 -0300

    component pass hijo a padre

--- components/hijo.js
+++ components/hijo.js
@@ -4,7 +4,14 @@ Vue.component('hijo',{
     <div class="py-5 p-5 bg-success">
         <h4>componente hijo</h4>
         <p>{{ numero }}</p>
+        <h3>{{ nombre }}</h3>
     </div>
     `,
-    props: ['numero']
+    props: ['numero'],
+    data(){
+        return { nombre: 'franco' }
+    },
+    mounted() {
+        this.$emit('nombreHijo', this.nombre)
+    },
 })
--- components/padre.js
+++ components/padre.js
@@ -4,11 +4,15 @@ Vue.component('padre', {
     <div class="p-5 bg-dark text-white">
         <h2>componente padre: {{ numeroPadre }}</h2>
         <button class="btn-danger" @click="numeroPadre++">Sumar </button>
-        <hijo :numero="numeroPadre"></hijo>
+        {{ nombrePadre }}
+        <hijo :numero="numeroPadre" @nombreHijo="nombrePadre = $event"></hijo>
     </div>
     `,
     data() {
-        return { numeroPadre: 0 }
+        return { 
+            numeroPadre: 0,
+            nombrePadre: ''
+         }
     }
 
 })
